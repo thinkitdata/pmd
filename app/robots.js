8a88c0ae8fe6e16817b71f113e8e54b822d18920
thinkitdata <greg@thinkitdata.com>
2026-08-17 15:55:59 -0400

    Fix static export: force-static on sitemap/robots, pin Node 22 in Amplify build

--- app/robots.js
+++ app/robots.js
@@ -1,5 +1,7 @@
 import { site } from "@/site.config";
 
+export const dynamic = "force-static";
+
 const BASE = process.env.NEXT_PUBLIC_SITE_URL || site.url;
 
 export default function robots() {
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,5 +1,7 @@
 import { site, services } from "@/site.config";
 
+export const dynamic = "force-static";
+
 const BASE = process.env.NEXT_PUBLIC_SITE_URL || site.url;
 
 export default function sitemap() {
